68540db90a2ddb1e685c022da3c66ceff573430d
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-20 20:23:47 +0700

    feat: add new dependencies for user service

--- internal/service/service.go
+++ internal/service/service.go
@@ -8,8 +8,11 @@ import (
 )
 
 type Service struct {
+	UserService IUserService
 }
 
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, supabase supabase.Interface) *Service {
-	return &Service{}
+	return &Service{
+		UserService: NewUserService(repository.UserRepository, repository.CartRepository, repository.AddressRepository, bcrypt, jwtAuth, supabase),
+	}
 }
