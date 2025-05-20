f651479023e7060bacf6ba4ac2847a1e54b1bea2
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-20 20:24:44 +0700

    feat: add new function to create cart for user register function

---
+++ internal/repository/cart.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"ikan-nusa/entity"
+
+	"gorm.io/gorm"
+)
+
+type ICartRepository interface {
+	CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Cart, error)
+}
+
+type CartRepository struct {
+	db *gorm.DB
+}
+
+func NewCartRepository(db *gorm.DB) ICartRepository {
+	return &CartRepository{db: db}
+}
+
+func (c *CartRepository) CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Cart, error) {
+	err := tx.Debug().Create(&cart).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return cart, nil
+}
