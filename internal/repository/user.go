df6a51d6e47db43ca5bc6c5e854ed28db0282879
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-20 20:21:31 +0700

    feat: add function to create new user

---
+++ internal/repository/user.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"ikan-nusa/entity"
+	"ikan-nusa/model"
+
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	CreateUser(tx *gorm.DB, user *entity.User) (*entity.User, error)
+	GetUser(param model.UserParam) (*entity.User, error)
+}
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &UserRepository{db: db}
+}
+
+func (u *UserRepository) CreateUser(tx *gorm.DB, user *entity.User) (*entity.User, error) {
+	err := tx.Debug().Create(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *UserRepository) GetUser(param model.UserParam) (*entity.User, error) {
+	user := entity.User{}
+	err := u.db.Debug().Preload("Address.District.City.Province").Where(&param).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
