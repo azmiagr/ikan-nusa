da8eda5161a8831140a9b637a06af9cec66791c2
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-20 17:47:33 +0700

    feat: add entity to migrate to database

--- pkg/database/mariadb/migrate.go
+++ pkg/database/mariadb/migrate.go
@@ -1,11 +1,21 @@
 package mariadb
 
 import (
+	"ikan-nusa/entity"
+
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
-	err := db.AutoMigrate()
+	err := db.AutoMigrate(
+		&entity.User{},
+		&entity.OtpCode{},
+		&entity.Cart{},
+		&entity.Province{},
+		&entity.City{},
+		&entity.District{},
+		&entity.Address{},
+	)
 	if err != nil {
 		return err
 	}
