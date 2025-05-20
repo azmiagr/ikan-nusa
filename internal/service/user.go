fe2a870fb6a8d6f48b0ca87d4cc123c3f1a21e3a
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-20 21:31:14 +0700

    feat: add new function to add address after user register and function to verify user with otp

--- internal/service/user.go
+++ internal/service/user.go
@@ -8,7 +8,11 @@ import (
 	"ikan-nusa/pkg/bcrypt"
 	"ikan-nusa/pkg/database/mariadb"
 	"ikan-nusa/pkg/jwt"
+	"ikan-nusa/pkg/mail"
 	"ikan-nusa/pkg/supabase"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -16,6 +20,8 @@ import (
 
 type IUserService interface {
 	Register(param *model.UserRegister) (*model.UserRegisterResponse, error)
+	AddAddressAfterRegister(param model.AddAddressAfterRegisterParam) error
+	VerifyUser(param model.VerifyUser) error
 }
 
 type UserService struct {
@@ -23,17 +29,19 @@ type UserService struct {
 	UserRepository    repository.IUserRepository
 	CartRepository    repository.ICartRepository
 	AddressRepository repository.IAddressRepository
+	OtpRepository     repository.IOtpRepository
 	BCrypt            bcrypt.Interface
 	JwtAuth           jwt.Interface
 	Supabase          supabase.Interface
 }
 
-func NewUserService(userRepository repository.IUserRepository, cartRepository repository.ICartRepository, addressRepository repository.IAddressRepository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, supabase supabase.Interface) IUserService {
+func NewUserService(userRepository repository.IUserRepository, cartRepository repository.ICartRepository, addressRepository repository.IAddressRepository, otpRepository repository.IOtpRepository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, supabase supabase.Interface) IUserService {
 	return &UserService{
 		db:                mariadb.Connection,
 		UserRepository:    userRepository,
 		CartRepository:    cartRepository,
 		AddressRepository: addressRepository,
+		OtpRepository:     otpRepository,
 		BCrypt:            bcrypt,
 		JwtAuth:           jwtAuth,
 		Supabase:          supabase,
@@ -99,4 +107,104 @@ func (u *UserService) Register(param *model.UserRegister) (*model.UserRegisterRe
 
 	return &result, nil
 
+}
+
+func (u *UserService) AddAddressAfterRegister(param model.AddAddressAfterRegisterParam) error {
+	tx := u.db.Begin()
+	defer tx.Rollback()
+
+	user, err := u.UserRepository.GetUser(model.UserParam{
+		UserID: param.UserID,
+	})
+	if err != nil {
+		return err
+	}
+
+	address := &entity.Address{
+		RecipentName:  user.Username,
+		PostalCode:    param.PostalCode,
+		AddressDetail: param.AddressDetail,
+		DistrictID:    param.DistrictID,
+		UserID:        user.UserID,
+	}
+
+	_, err = u.AddressRepository.CreateAddress(tx, address)
+	if err != nil {
+		return err
+	}
+
+	code := mail.GenerateCode()
+	otp := &entity.OtpCode{
+		OtpID:  uuid.New(),
+		UserID: user.UserID,
+		Code:   code,
+	}
+
+	err = u.OtpRepository.CreateOtp(tx, otp)
+	if err != nil {
+		return err
+	}
+
+	err = mail.SendEmail(user.Email, "OTP Verification", "Your OTP verification code is "+code+".")
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *UserService) VerifyUser(param model.VerifyUser) error {
+	tx := u.db.Begin()
+	defer tx.Rollback()
+
+	otp, err := u.OtpRepository.GetOtp(tx, model.GetOtp{
+		UserID: param.UserID,
+	})
+	if err != nil {
+		return err
+	}
+
+	if otp.Code != param.OtpCode {
+		return errors.New("invalid otp code")
+	}
+
+	expiredTime, err := strconv.Atoi(os.Getenv("EXPIRED_OTP"))
+	if err != nil {
+		return err
+	}
+
+	expiredThreshold := time.Now().UTC().Add(-time.Duration(expiredTime) * time.Minute)
+	if otp.UpdatedAt.Before(expiredThreshold) {
+		return errors.New("otp expired")
+	}
+
+	user, err := u.UserRepository.GetUser(model.UserParam{
+		UserID: param.UserID,
+	})
+	if err != nil {
+		return err
+	}
+
+	user.StatusAccount = "active"
+	err = u.UserRepository.UpdateUser(tx, user)
+	if err != nil {
+		return err
+	}
+
+	err = u.OtpRepository.DeleteOtp(tx, otp)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
