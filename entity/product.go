2c9d0bf0e4a7edb26ca7a741796dfd2ccf3218c5
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-22 00:58:30 +0700

    feat: add product as new entity

---
+++ entity/product.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ProductID          int       `json:"product_id" gorm:"type:int;primaryKey;autoIncrement"`
+	ProductName        string    `json:"product_name" gorm:"type:varchar(65);not null"`
+	ProductDescription string    `json:"product_description" gorm:"type:text;not null"`
+	Price              float64   `json:"price" gorm:"type:decimal;not null"`
+	Stock              int       `json:"stock" gorm:"type:int;not null"`
+	ImageURL           string    `json:"image_url" gorm:"text"`
+	CreatedAt          time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt          time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	StoreID            uuid.UUID `json:"store_id"`
+}
