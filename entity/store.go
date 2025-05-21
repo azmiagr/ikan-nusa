bf7c1b767ef141e0d2178bc355a3dfbedc36c859
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-22 00:58:52 +0700

    feat: add store as new entity

---
+++ entity/store.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Store struct {
+	StoreID          uuid.UUID `json:"store_id" gorm:"type:varchar(36);primaryKey"`
+	StoreName        string    `json:"store_name" gorm:"type:varchar(90);not null"`
+	StoreDescription string    `json:"store_description" gorm:"type:text;not null"`
+	CreatedAt        time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt        time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	UserID           uuid.UUID `json:"user_id" `
+
+	Products []Product `json:"products" gorm:"foreignKey:StoreID"`
+}
