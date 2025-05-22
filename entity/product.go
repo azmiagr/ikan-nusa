43d830cbf1a871eb0061efe5dc555bc0a1972196
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-22 15:23:33 +0700

    refactor: add category attributes to product entity

--- entity/product.go
+++ entity/product.go
@@ -12,6 +12,7 @@ type Product struct {
 	ProductDescription string    `json:"product_description" gorm:"type:text;not null"`
 	Price              float64   `json:"price" gorm:"type:decimal;not null"`
 	Stock              int       `json:"stock" gorm:"type:int;not null"`
+	Category           string    `json:"category" gorm:"enum('air tawar', 'air laut', 'air payau');"`
 	ImageURL           string    `json:"image_url" gorm:"text"`
 	CreatedAt          time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt          time.Time `json:"updated_at" gorm:"autoUpdateTime"`
