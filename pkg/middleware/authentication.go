65eb5704cb948af9ea84fe5f6aba02ddc45f4736
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-22 15:20:44 +0700

    feat: add function to authenticate user

---
+++ pkg/middleware/authentication.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"ikan-nusa/model"
+	"ikan-nusa/pkg/response"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (m *middleware) AuthenticateUser(c *gin.Context) {
+	bearer := c.GetHeader("Authorization")
+	if bearer == "" {
+		response.Error(c, http.StatusUnauthorized, "empty token", nil)
+		c.Abort()
+		return
+	}
+
+	token := strings.Split(bearer, " ")[1]
+	userID, err := m.jwtAuth.ValidateToken(token)
+	if err != nil {
+		response.Error(c, http.StatusUnauthorized, "failed to validate token", err)
+		c.Abort()
+		return
+	}
+
+	user, err := m.service.UserService.GetUser(model.UserParam{
+		UserID: userID,
+	})
+	if err != nil {
+		response.Error(c, http.StatusUnauthorized, "failed to get user", err)
+		c.Abort()
+		return
+	}
+
+	c.Set("user", user)
+	c.Next()
+}
