adde96d714d3eaa188046a731fe647d38ec470bb
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-22 15:25:13 +0700

    refactor: injecting store repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -6,6 +6,7 @@ type Repository struct {
 	UserRepository    IUserRepository
 	CartRepository    ICartRepository
 	AddressRepository IAddressRepository
+	StoreRepository   IStoreRepository
 	OtpRepository     IOtpRepository
 }
 
@@ -14,6 +15,7 @@ func NewRepository(db *gorm.DB) *Repository {
 		UserRepository:    NewUserRepository(db),
 		CartRepository:    NewCartRepository(db),
 		AddressRepository: NewAddressRepository(db),
+		StoreRepository:   NewStoreRepository(db),
 		OtpRepository:     NewOtpRepository(db),
 	}
 }
