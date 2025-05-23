5944f62779eb49d1768bdd42177c80df0d326af9
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-23 21:41:03 +0700

    feat: add new struct for get product detail response

--- model/product.go
+++ model/product.go
@@ -17,3 +17,12 @@ type GetProductsByCategoryResponse struct {
 	Price       float64 `json:"price"`
 	StoreName   string  `json:"store_name"`
 }
+
+type GetProductsDetailResponse struct {
+	ProductName string  `json:"product_name"`
+	Price       float64 `json:"price"`
+	Stock       int     `json:"stock"`
+	Description string  `json:"description"`
+	Category    string  `json:"category"`
+	StoreName   string  `json:"store_name"`
+}
