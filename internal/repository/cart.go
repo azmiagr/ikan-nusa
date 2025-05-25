025ed9de516a252d879fdae98d3faf4341b0871e
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-25 17:30:26 +0700

    feat: add get cart by id function

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -8,6 +8,7 @@ import (
 
 type ICartRepository interface {
 	CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Cart, error)
+	GetCartByID(tx *gorm.DB, cartID int) (*entity.Cart, error)
 }
 
 type CartRepository struct {
@@ -20,6 +21,16 @@ func NewCartRepository(db *gorm.DB) ICartRepository {
 
 func (c *CartRepository) CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Cart, error) {
 	err := tx.Debug().Create(&cart).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return cart, nil
+}
+
+func (c *CartRepository) GetCartByID(tx *gorm.DB, cartID int) (*entity.Cart, error) {
+	var cart *entity.Cart
+	err := tx.Debug().Where("cart_id = ?", cartID).Find(&cart).Error
 	if err != nil {
 		return nil, err
 	}
