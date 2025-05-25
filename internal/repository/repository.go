b2fb2e04cc036274e5811e18b58d2e7bf9abcc87
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-25 17:32:59 +0700

    refactor: add cart items repository dependencies

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,21 +3,23 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	UserRepository    IUserRepository
-	CartRepository    ICartRepository
-	AddressRepository IAddressRepository
-	StoreRepository   IStoreRepository
-	OtpRepository     IOtpRepository
-	ProductRepository IProductRepository
+	UserRepository      IUserRepository
+	CartRepository      ICartRepository
+	AddressRepository   IAddressRepository
+	StoreRepository     IStoreRepository
+	OtpRepository       IOtpRepository
+	ProductRepository   IProductRepository
+	CartItemsRepository ICartItemsRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository:    NewUserRepository(db),
-		CartRepository:    NewCartRepository(db),
-		AddressRepository: NewAddressRepository(db),
-		StoreRepository:   NewStoreRepository(db),
-		ProductRepository: NewProductRepository(db),
-		OtpRepository:     NewOtpRepository(db),
+		UserRepository:      NewUserRepository(db),
+		CartRepository:      NewCartRepository(db),
+		AddressRepository:   NewAddressRepository(db),
+		StoreRepository:     NewStoreRepository(db),
+		ProductRepository:   NewProductRepository(db),
+		OtpRepository:       NewOtpRepository(db),
+		CartItemsRepository: NewCartItemsRepository(db),
 	}
 }
