72fe6f6c03b2093a49f1da229401e291baf12d81
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-28 12:39:17 +0700

    refactor: injecting supabase to product service

--- internal/service/service.go
+++ internal/service/service.go
@@ -17,7 +17,7 @@ type Service struct {
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, supabase supabase.Interface) *Service {
 	return &Service{
 		UserService:      NewUserService(repository.UserRepository, repository.CartRepository, repository.AddressRepository, repository.OtpRepository, repository.StoreRepository, bcrypt, jwtAuth, supabase),
-		ProductService:   NewProductService(repository.ProductRepository, repository.StoreRepository),
+		ProductService:   NewProductService(repository.ProductRepository, repository.StoreRepository, supabase),
 		CartItemsService: NewCartItemsService(repository.UserRepository, repository.CartItemsRepository, repository.CartRepository, repository.ProductRepository, repository.StoreRepository),
 		ReviewService:    NewReviewService(repository.ReviewRepository, repository.UserRepository),
 	}
