8903c87c859adb01df10d172c47154235a670c45
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-29 20:27:01 +0700

    refactor: add product type repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,25 +3,27 @@ package repository
 import "gorm.io/gorm"
 
 type Repository struct {
-	UserRepository      IUserRepository
-	CartRepository      ICartRepository
-	AddressRepository   IAddressRepository
-	StoreRepository     IStoreRepository
-	OtpRepository       IOtpRepository
-	ProductRepository   IProductRepository
-	CartItemsRepository ICartItemsRepository
-	ReviewRepository    IReviewRepository
+	UserRepository        IUserRepository
+	CartRepository        ICartRepository
+	AddressRepository     IAddressRepository
+	StoreRepository       IStoreRepository
+	OtpRepository         IOtpRepository
+	ProductRepository     IProductRepository
+	CartItemsRepository   ICartItemsRepository
+	ReviewRepository      IReviewRepository
+	ProductTypeRepository IProductTypeRepository
 }
 
 func NewRepository(db *gorm.DB) *Repository {
 	return &Repository{
-		UserRepository:      NewUserRepository(db),
-		CartRepository:      NewCartRepository(db),
-		AddressRepository:   NewAddressRepository(db),
-		StoreRepository:     NewStoreRepository(db),
-		ProductRepository:   NewProductRepository(db),
-		OtpRepository:       NewOtpRepository(db),
-		CartItemsRepository: NewCartItemsRepository(db),
-		ReviewRepository:    NewReviewRepository(db),
+		UserRepository:        NewUserRepository(db),
+		CartRepository:        NewCartRepository(db),
+		AddressRepository:     NewAddressRepository(db),
+		StoreRepository:       NewStoreRepository(db),
+		ProductRepository:     NewProductRepository(db),
+		OtpRepository:         NewOtpRepository(db),
+		CartItemsRepository:   NewCartItemsRepository(db),
+		ReviewRepository:      NewReviewRepository(db),
+		ProductTypeRepository: NewProductTypeRepository(db),
 	}
 }
