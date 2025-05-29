65c3674d6338ab13c47c7f4c21fad113b60bfa76
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-29 22:10:08 +0700

    feat: add new function to get cart by user id

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -3,12 +3,14 @@ package repository
 import (
 	"ikan-nusa/entity"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type ICartRepository interface {
 	CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Cart, error)
 	GetCartByID(tx *gorm.DB, cartID int) (*entity.Cart, error)
+	GetCartByUserID(tx *gorm.DB, userID uuid.UUID) (*entity.Cart, error)
 }
 
 type CartRepository struct {
@@ -31,6 +33,16 @@ func (c *CartRepository) CreateCart(tx *gorm.DB, cart *entity.Cart) (*entity.Car
 func (c *CartRepository) GetCartByID(tx *gorm.DB, cartID int) (*entity.Cart, error) {
 	var cart *entity.Cart
 	err := tx.Debug().Where("cart_id = ?", cartID).Find(&cart).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return cart, nil
+}
+
+func (c *CartRepository) GetCartByUserID(tx *gorm.DB, userID uuid.UUID) (*entity.Cart, error) {
+	var cart *entity.Cart
+	err := tx.Debug().Where("user_id = ?", userID).Find(&cart).Error
 	if err != nil {
 		return nil, err
 	}
