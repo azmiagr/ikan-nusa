40f0ebde1429b7a2c6ee47f9128360b145199cf8
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-05-30 19:25:56 +0700

    feat: add new function to user address

--- internal/repository/address.go
+++ internal/repository/address.go
@@ -2,12 +2,14 @@ package repository
 
 import (
 	"ikan-nusa/entity"
+	"ikan-nusa/model"
 
 	"gorm.io/gorm"
 )
 
 type IAddressRepository interface {
 	CreateAddress(tx *gorm.DB, address *entity.Address) (*entity.Address, error)
+	GetAddress(tx *gorm.DB, param model.AddressParam) (*entity.Address, error)
 }
 
 type AddressRepository struct {
@@ -20,6 +22,16 @@ func NewAddressRepository(db *gorm.DB) IAddressRepository {
 
 func (a *AddressRepository) CreateAddress(tx *gorm.DB, address *entity.Address) (*entity.Address, error) {
 	err := tx.Debug().Create(&address).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return address, nil
+}
+
+func (a *AddressRepository) GetAddress(tx *gorm.DB, param model.AddressParam) (*entity.Address, error) {
+	var address *entity.Address
+	err := tx.Debug().Preload("District.City.Province").Where(&param).First(&address).Error
 	if err != nil {
 		return nil, err
 	}
