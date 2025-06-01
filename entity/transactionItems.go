6c44dfa33fc27eebf4982e24a09394cb7f12bd68
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-01 18:24:30 +0700

    refactor: add total unit price to attributes and minor adjustment to created at attributes name

--- entity/transactionItems.go
+++ entity/transactionItems.go
@@ -6,7 +6,8 @@ type TransactionItems struct {
 	TransactionItemsID int       `json:"transaction_items_id" gorm:"type:int;primaryKey;autoIncrement"`
 	Quantity           int       `json:"quantity" gorm:"type:int;not null"`
 	UnitPrice          float64   `json:"unit_price" gorm:"type:decimal;not null"`
-	CreateedAt         time.Time `json:"created_at" gorm:"autoCreateTime;not null"`
+	TotalUnitPrice     float64   `json:"total_unit_price" gorm:"type:decimal;not null"`
+	CreatedAt          time.Time `json:"created_at" gorm:"autoCreateTime;not null"`
 	UpdatedAt          time.Time `json:"updated_at" gorm:"autoUpdateTime;not null"`
 	TransactionID      int       `json:"transaction_id"`
 	ProductID          int       `json:"product_id"`
