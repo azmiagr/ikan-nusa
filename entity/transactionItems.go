1f43150260367e32652d0d87f3dfc5e92f3786c0
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-01 15:28:10 +0700

    feat: add transaction items entity for transaction and product many to many relations

---
+++ entity/transactionItems.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type TransactionItems struct {
+	TransactionItemsID int       `json:"transaction_items_id" gorm:"type:int;primaryKey;autoIncrement"`
+	Quantity           int       `json:"quantity" gorm:"type:int;not null"`
+	UnitPrice          float64   `json:"unit_price" gorm:"type:decimal;not null"`
+	CreateedAt         time.Time `json:"created_at" gorm:"autoCreateTime;not null"`
+	UpdatedAt          time.Time `json:"updated_at" gorm:"autoUpdateTime;not null"`
+	TransactionID      int       `json:"transaction_id"`
+	ProductID          int       `json:"product_id"`
+}
