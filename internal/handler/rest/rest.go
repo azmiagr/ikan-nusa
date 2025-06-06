527222979e296caaefbc76720b0114b7df86ca42
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-06 23:30:14 +0700

    refactor: add CORS handling on endpoints

--- internal/handler/rest/rest.go
+++ internal/handler/rest/rest.go
@@ -25,6 +25,7 @@ func NewRest(service *service.Service, middleware middleware.Interface) *Rest {
 
 func (r *Rest) MountEndpoint() {
 	baseURL := r.router.Group("/api/v1")
+	baseURL.Use(r.middleware.Cors())
 	baseURL.GET("/store-details/:store_name", r.GetStoreDetail)
 
 	products := baseURL.Group("/products")
