38870d00a6004c7add3b442386efaa25dbe87b88
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-07 20:18:19 +0700

    feat: add new struct for get city and districts responses

--- model/address.go
+++ model/address.go
@@ -5,3 +5,13 @@ import "github.com/google/uuid"
 type AddressParam struct {
 	UserID uuid.UUID `json:"user_id"`
 }
+
+type GetCityResponse struct {
+	CityID   int    `json:"city_id"`
+	CityName string `json:"city_name"`
+}
+
+type GetDistrictResponse struct {
+	DistrictID   int    `json:"district_id"`
+	DistrictName string `json:"district_name"`
+}
