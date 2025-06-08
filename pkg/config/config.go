ed629ca8d90e9582817946f7441f2e0df1c659f4
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-08 23:24:54 +0700

    refactor: small adjustment for Railways

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,15 +1,11 @@
 package config
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvironment() error {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Fatalf("failed to load env, err %v", err)
-	}
+	_ = godotenv.Load("../../.env")
+
 	return nil
 }
