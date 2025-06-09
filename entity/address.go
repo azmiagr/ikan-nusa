809051404286d3ce747b816b1d709d7b43a754df
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-09 22:07:42 +0700

    refactor: removing postal code attributes from address

--- entity/address.go
+++ entity/address.go
@@ -9,8 +9,9 @@ import (
 type Address struct {
 	AddressID     uuid.UUID `json:"address_id" gorm:"type:varchar(36);primaryKey"`
 	RecipentName  string    `json:"recipent_name" gorm:"type:varchar(70);not null"`
-	PostalCode    string    `json:"postal_code" gorm:"type:varchar(5);not null"`
+	Label         string    `json:"label" gorm:"type:varchar(50);not null"`
 	AddressDetail string    `json:"address_detail" gorm:"type:varchar(100);not null"`
+	Notes         string    `json:"notes" gorm:"type:varchar(30);default:null"`
 	UserID        uuid.UUID `json:"user_id"`
 	CreatedAt     time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt     time.Time `json:"updated_at" gorm:"autoUpdateTime"`
