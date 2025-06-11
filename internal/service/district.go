8f8fe6e026924ffe92aaac6c90c9948a6412e176
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-11 13:13:42 +0700

    feat: add new function to handle response for get districts by city id

--- internal/service/district.go
+++ internal/service/district.go
@@ -7,6 +7,7 @@ import (
 
 type IDistrictService interface {
 	GetAllDistricts() ([]*model.GetDistrictResponse, error)
+	GetDistrictByCityId(cityID int) ([]*model.GetDistrictResponse, error)
 }
 
 type DistrictService struct {
@@ -23,6 +24,24 @@ func (d *DistrictService) GetAllDistricts() ([]*model.GetDistrictResponse, error
 	var res []*model.GetDistrictResponse
 
 	districts, err := d.DistrictRepository.GetAllDistricts()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range districts {
+		res = append(res, &model.GetDistrictResponse{
+			DistrictID:   v.DistrictID,
+			DistrictName: v.DistrictName,
+		})
+	}
+
+	return res, nil
+}
+
+func (d *DistrictService) GetDistrictByCityId(cityID int) ([]*model.GetDistrictResponse, error) {
+	var res []*model.GetDistrictResponse
+
+	districts, err := d.DistrictRepository.GetDistrictByCityId(cityID)
 	if err != nil {
 		return nil, err
 	}
