8e52d91178952da37c27324efaa8c5bc223af1f3
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-11 16:55:21 +0700

    feat: add new function to get product reviews

--- internal/handler/rest/review.go
+++ internal/handler/rest/review.go
@@ -5,6 +5,7 @@ import (
 	"ikan-nusa/model"
 	"ikan-nusa/pkg/response"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -26,4 +27,21 @@ func (r *Rest) AddReview(c *gin.Context) {
 	}
 
 	response.Success(c, http.StatusOK, "success add review", res)
+}
+
+func (r *Rest) GetReviewByProductID(c *gin.Context) {
+	idStr := c.Param("product_id")
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid product id", err)
+		return
+	}
+
+	res, err := r.service.ReviewService.GetReviewByProductID(idInt)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to get reviews", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success to get reviews", res)
 }
