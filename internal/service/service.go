9942a119ab770014a2187f82f5a06b8747991258
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-11 17:16:14 +0700

    feat: injecting product type repository to product service

--- internal/service/service.go
+++ internal/service/service.go
@@ -22,7 +22,7 @@ type Service struct {
 func NewService(repository *repository.Repository, bcrypt bcrypt.Interface, jwtAuth jwt.Interface, supabase supabase.Interface) *Service {
 	return &Service{
 		UserService:        NewUserService(repository.UserRepository, repository.CartRepository, repository.AddressRepository, repository.OtpRepository, repository.StoreRepository, repository.ProductRepository, repository.CartItemsRepository, repository.TransactionRepository, bcrypt, jwtAuth, supabase),
-		ProductService:     NewProductService(repository.ProductRepository, repository.StoreRepository, supabase),
+		ProductService:     NewProductService(repository.ProductTypeRepository, repository.ProductRepository, repository.StoreRepository, supabase),
 		CartItemsService:   NewCartItemsService(repository.UserRepository, repository.CartItemsRepository, repository.CartRepository, repository.ProductRepository, repository.StoreRepository),
 		ReviewService:      NewReviewService(repository.ReviewRepository, repository.UserRepository, repository.ProductRepository),
 		ProductTypeService: NewProductTypeService(repository.ProductTypeRepository),
