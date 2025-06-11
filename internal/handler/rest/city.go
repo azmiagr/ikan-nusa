d74aecfb570212db0181b89934dd7c6bc903a3dd
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-11 13:11:31 +0700

    feat: add new function to get city by their province id

--- internal/handler/rest/city.go
+++ internal/handler/rest/city.go
@@ -3,12 +3,30 @@ package rest
 import (
 	"ikan-nusa/pkg/response"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func (r *Rest) GetAllCities(c *gin.Context) {
 	res, err := r.service.CityService.GetAllCities()
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to get cities data", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success to get cities data", res)
+}
+
+func (r *Rest) GetCitiesByProvinceID(c *gin.Context) {
+	idStr := c.Param("province_id")
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid province id", err)
+		return
+	}
+
+	res, err := r.service.CityService.GetCitiesByProvinceID(idInt)
 	if err != nil {
 		response.Error(c, http.StatusInternalServerError, "failed to get cities data", err)
 		return
