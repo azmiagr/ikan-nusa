a8f35369a66331165b3f3be35481f8469b29d444
Azmi Al Ghifari Rahman <azmiagr@gmail.com>
2025-06-11 13:14:01 +0700

    feat: add new function to get district by their city id

--- internal/handler/rest/district.go
+++ internal/handler/rest/district.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"ikan-nusa/pkg/response"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,23 @@ func (r *Rest) GetAllDistricts(c *gin.Context) {
 	res, err := r.service.DistrictService.GetAllDistricts()
 	if err != nil {
 		response.Error(c, http.StatusInternalServerError, "failed to get districts data", err)
+		return
+	}
+
+	response.Success(c, http.StatusOK, "success to get districts data", res)
+}
+
+func (r *Rest) GetDistrictByCityId(c *gin.Context) {
+	idStr := c.Param("city_id")
+	idInt, err := strconv.Atoi(idStr)
+	if err != nil {
+		response.Error(c, http.StatusBadRequest, "invalid city id", err)
+		return
+	}
+
+	res, err := r.service.DistrictService.GetDistrictByCityId(idInt)
+	if err != nil {
+		response.Error(c, http.StatusInternalServerError, "failed to get districts", err)
 		return
 	}
 
